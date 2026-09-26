698732c445ba67de716d47185549f33036398565
Rinconi <tabletrinconi@gmail.com>
2026-09-26 02:01:09 +0200

    Add new match data for Girona and Albacete

--- main2division.py
+++ main2division.py
@@ -119,3 +119,4 @@ nuevos = fetch_laliga_oficial_desde(ultima_fecha)
 if len(PARTIDOS.get("girona",[]))==6:
     PARTIDOS["girona"].append(("2026-09-25","Girona 2-0 Albacete","V","","2-0"))
     PARTIDOS["albacete"].append(("2026-09-25","","D","Girona 2-0 Albaceta", "2-0"))
+    nuevos+=1
